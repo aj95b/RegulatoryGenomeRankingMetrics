6eccb2a71d76226d215f673567be66aabcac44da
Arpita <arpitamhow@gmail.com>
2023-02-12 21:21:37 -0800

    Create zscore_metric_call_wise.py

---
+++ information_metrics/zscore_metric_call_wise.py
@@ -0,0 +1,12 @@
+from read_data_global_variables import *
+
+#After executing the function, sort the file with dhs_index, to obtain z-scoring with genomic index wise
+def zscore_metric_k_wise(metric_file): #Path to file containing metric score for each DHS
+	 metric = pd.read_csv(metric_file,sep='\t')
+	 print('dhs_index'+'\t'+'z-scored_fix_k')
+	 for i in range(1,biosamples+1):
+	 	len_k = len(metric.iloc[np.where(calls==i)[0],0])
+	 	x = pd.DataFrame(data=stats.zscore(metric.iloc[np.where(calls==i)[0],0]))
+	 	for j in range(len_k):
+	 		print(x.index[j],end='\t')
+	 		print(x.iloc[j,0])
